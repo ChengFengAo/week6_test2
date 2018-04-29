990479d08207d3e6c03306b0cc3c77b9f88e979f
ChengFeng AO <414356022@qq.com>
2018-04-29 14:15:49 +0800

    use employeeController to return data to employee.jsp

--- src/main/java/com/example/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapi/controller/EmployeeController.java
@@ -1,25 +1,30 @@
 package com.example.employeeapi.controller;
 
 import com.example.employeeapi.datasource.EmployeeRepository;
+import com.example.employeeapi.datasource.TestEmployeeRepository;
+import com.example.employeeapi.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
-
     @RequestMapping("/employees")
-    String getEmployees(Model model) {
-
-        model.addAttribute("message", "hello world");
-
+    String getEmployees(Model  modle) {
+        modle.addAttribute("getAllEmployee", employeeRepository.getAllEmployees());
         return "employees";
     }
 
